bd6574fccdbccb19f5f2b3245018f8b15c5e5de7
Karolina Tomaszewska <k.tomaszewska.97@gmail.com>
2022-03-29 14:26:59 +0200

    Implemented POST request

--- io.openems.edge.newdevice/src/io/openems/edge/newdevice/NewDeviceImpl.java
+++ io.openems.edge.newdevice/src/io/openems/edge/newdevice/NewDeviceImpl.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import org.osgi.service.component.ComponentContext;
@@ -70,6 +71,7 @@ public class NewDeviceImpl extends AbstractOpenemsComponent implements NewDevice
 				counter = 0;
 				try {
 					this.sendGet();
+					//this.sendPost();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					if (this.config.logsEnabled()) {
@@ -128,6 +130,44 @@ public class NewDeviceImpl extends AbstractOpenemsComponent implements NewDevice
 				System.out.println("JsonArray:" + JsonParser.parseString(response.toString()).isJsonArray());
 			}
 		}
+	}
+
+	private void sendPost() throws IOException {
+		URL obj = new URL(this.config.ssbUrl());
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		//con.setRequestProperty("Content-Type", "application/json");
+		con.setDoOutput(true);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("POST Response Code :  " + responseCode);
+		System.out.println("POST Response Message : " + con.getResponseMessage());
+
+		if (responseCode == HttpURLConnection.HTTP_CREATED) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			if (JsonParser.parseString(response.toString()).isJsonObject()) {
+				JsonObject jo = JsonParser.parseString(response.toString()).getAsJsonObject();
+				System.out.println(jo);
+				this._setArrayVoltage(jo.get("arrayVoltage").getAsDouble());
+				this._setLoadPower(jo.get("loadPower").getAsDouble());
+				System.out.println(this.getArrayVoltageChannel());
+				System.out.println(this.getLoadPowerChannel());
+			} else {
+				System.out.println("Not JsonObject");
+				System.out.println("JsonArray:" + JsonParser.parseString(response.toString()).isJsonArray());
+			}
+			System.out.println(response.toString());
+		} else {
+			System.out.println("POST NOT WORKED");
+		}
+
 	}
 
 }
