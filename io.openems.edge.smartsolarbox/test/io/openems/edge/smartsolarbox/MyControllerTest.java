dcf323f9a39920256553f91139fab4831ecb05e2
Karolina Tomaszewska <k.tomaszewska.97@gmail.com>
2022-04-29 14:29:34 +0200

    deleted test in smartsolarbox

--- io.openems.edge.smartsolarbox/test/io/openems/edge/smartsolarbox/MyControllerTest.java
+++ io.openems.edge.smartsolarbox/test/io/openems/edge/smartsolarbox/MyControllerTest.java
@@ -7,15 +7,4 @@ import io.openems.edge.controller.test.ControllerTest;
 
 public class MyControllerTest {
 
-	private static final String CTRL_ID = "ctrl0";
-
-	@Test
-	public void test() throws Exception {
-		new ControllerTest(new SmartSolarBoxImpl()) //
-				.activate(MyConfig.create() //
-						.setId(CTRL_ID) //
-						.build())
-				.next(new TestCase());
-	}
-
 }
